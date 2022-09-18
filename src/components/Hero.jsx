26e0caba89a4d8e5ad6cbe15f4bde77486bf4b59
yurias-ramdhan <yuriasramdhan@gmail.com>
2022-09-18 13:17:04 +0700

    mengubah dikit isi dari hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import Typed from "typed.js";
+import React, { useEffect, useRef } from "react";
 
 const Hero = () => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Yurias Ramdhan"], // Strings to display
+      // Speed settings, try diffrent values untill you get good results
+      typeSpeed: 100,
+      backSpeed: 100,
+      loop: true,
+    });
+
+    // Destropying
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <section id="home" class="pt-36">
       <div class="container">
         <div class="flex flex-wrap">
           <div class="w-full self-center px-4 lg:w-1/2">
             <h1 class="text-base font-semibold text-primary md:text-xl">
-              Halo Semua, saya <span class="mt-3 block text-4xl text-dark lg:text-5xl">Yurias Ramdhan</span>
-            </h1>
+              Halo Semua, saya </h1>
+            <span class="mt-3 text-4xl text-dark lg:text-5xl" ref={el}></span>
+            
             <h2 class="mb-10 text-lg font-medium text-secondary lg:text-2xl">Calon Full Stack Developer</h2>
             <a href="#contact" class="rounded-full bg-primary py-3 px-8 text-base font-semibold text-white hover:opacity-80 hover:shadow-lg">
               Hubungi Saya
